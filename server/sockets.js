77ad4318b2ae0807eabe7e169a1fbf57999d522a
John Edwards <joedw1978@gmail.com>
2019-06-07 17:23:54 -0400

    first commit

---
+++ server/sockets.js
@@ -0,0 +1,12 @@
+module.exports = function(io) {
+
+    io.sockets.on('connection', function(socket) {
+       console.log('connected', socket.id)
+
+        socket.on('drip', () => {
+            socket.emit('drop');
+            console.log('ping recieved')
+        });
+
+    });
+};
